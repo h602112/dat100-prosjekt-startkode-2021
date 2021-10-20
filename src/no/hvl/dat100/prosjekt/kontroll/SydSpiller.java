ad89004c801486fb6d4eedf5350f312fe527c5eb
mariuslarsen <mariuslarsen@localhost>
2021-10-20 23:10:18 +0200

    Ferdig med implementasjon av first fit strategy for SYDSPILLER

--- src/no/hvl/dat100/prosjekt/kontroll/SydSpiller.java
+++ src/no/hvl/dat100/prosjekt/kontroll/SydSpiller.java
@@ -1,11 +1,15 @@
 package no.hvl.dat100.prosjekt.kontroll;
 
+import java.util.Random;
+
 import no.hvl.dat100.prosjekt.TODO;
+import no.hvl.dat100.prosjekt.kontroll.dommer.Dommer;
 import no.hvl.dat100.prosjekt.kontroll.dommer.Regler;
 import no.hvl.dat100.prosjekt.kontroll.spill.Handling;
 import no.hvl.dat100.prosjekt.kontroll.spill.HandlingsType;
 import no.hvl.dat100.prosjekt.kontroll.spill.Spillere;
 import no.hvl.dat100.prosjekt.modell.Kort;
+import no.hvl.dat100.prosjekt.modell.KortSamling;
 
 /**
  * Klasse som for å representere en vriåtter syd-spiller. Strategien er å lete
@@ -39,9 +43,32 @@ public class SydSpiller extends Spiller {
 
 		// TODO - START
 		/* first-fit strategi */
-	
-		throw new UnsupportedOperationException(TODO.method());
+
+		Kort[] hand = getHand().getAllekort();
+		
+		for (Kort kort : hand) {
+			if (Regler.kanLeggeNed(kort, topp)) {
+				return new Handling(HandlingsType.LEGGNED, kort);
+			}
+		}
+		
+		if (getAntallTrekk() < Regler.maksTrekk()) {
+			return new Handling(HandlingsType.TREKK, null);
+		} else {
+			return new Handling(HandlingsType.FORBI, null);
+		}
+		
+
+//		throw new UnsupportedOperationException(TODO.method());
 	
 		// TODO - END
+	}
+	
+	public static boolean atter (Kort kort) {
+		return kort.getVerdi() == 8;
+	}
+	
+	public static boolean kanLeggeNed(Kort kort, Kort topp) {
+		return kort.sammeFarge(topp) || kort.sammeVerdi(topp) || atter(kort);
 	}
 }
